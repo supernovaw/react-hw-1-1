ca5710bae37cb25f397eaf24fcd457c34f8c1372
supernovaw <ruszrj@gmail.com>
2022-02-24 12:38:37 -0800

    ShopItemFunc added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function ShopItemFunc({ item }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-content'>
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div class='description'>{item.descriptionFull}</div>
+      <div class='highlight-window mobile'><div class='highlight-overlay'></div></div>
+
+      <div class='divider'></div>
+
+      <div class='purchase-info'>
+        <div class='price'>{item.currency + item.price.toFixed(2)}</div>
+        <button>Add to cart</button>
+      </div>
+    </div>
+  )
+}
+
+function App() {
+  const item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: '£'
+  }
+
+  return (
+    <div className='container'>
+      <div className='background-element'>
+      </div>
+      <div className='highlight-window'>
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className='window'>
+        <ShopItemFunc item={item} />
+      </div>
     </div>
   );
 }
